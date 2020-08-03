7427121a6485f001147a9200f43e2d61055a7624
davide-amabile <davideamabile81@gmail.com>
2020-08-03 16:01:37 +0200

    password

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // chiedere il nome e creare una variabile
-var nome = promt("Come ti chiami?");
-document.getElementById('name').innerHTML = "nome";
+var nome = prompt("Come ti chiami?");
+document.getElementById('name').innerHTML = nome;
 // chiedre il cognome e creare una variabile
-
+var cognome = prompt("Il tuo cognome?");
+document.getElementById('second-name').innerHTML = cognome;
 // chiedereil colore e creareuna variabile
+var colore = prompt("Colore preferito?")
+document.getElementById('color').innerHTML = colore + "19";
