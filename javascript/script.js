71ecda3bf699550811a3d9e4eda5cb7764fb8f1d
davide-amabile <davideamabile81@gmail.com>
2020-08-03 15:38:11 +0200

    var nome

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+// chiedere il nome e creare una variabile
+var nome = promt("Come ti chiami?");
+document.getElementById('name').innerHTML = "nome";
+// chiedre il cognome e creare una variabile
+
+// chiedereil colore e creareuna variabile
